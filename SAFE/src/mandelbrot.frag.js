778d36269f216e9dfe0239015e9e525c377ea21c
prep0ster0us <leonie-moedl@web.de>
2021-12-09 18:33:19 +0100

    fixed the mandelbrot set - and created some accidental art!

--- SAFE/src/mandelbrot.frag.js
+++ SAFE/src/mandelbrot.frag.js
@@ -12,53 +12,26 @@ uniform vec3 parameterSet1;
 uniform vec3 parameterSet2;
 uniform vec3 color;
 
-vec2 complexMultiplikation (vec2 a, vec2 b){
-  return vec2(a.x*b.x - a.y*b.y, a.x*b.y + b.x*a.y);
-}
-
-float mandelbrot(vec2 complexNumber){
-  float alpha = 1.0;
-  vec2 z = vec2(0.0 , 0.0);
-  vec2 z_0;
-  vec2 z_1;
-  vec2 z_2;
-
-  for(int i=0; i < 200; i++){  // i < max iterations
-    z_2 = z_1;
-    z_1 = z_0;
-    z_0 = z;
-
-    float x_0_sq = z_0.x*z_0.x;
-    float y_0_sq = z_0.y*z_0.y;
-    vec2 z_0_sq = vec2(x_0_sq - y_0_sq, 2.0*z_0.x*z_0.y);
-    float x_1_sq = z_1.x*z_1.x;
-    float y_1_sq = z_1.y*z_1.y;
-    vec2 z_1_sq = vec2(x_1_sq - y_1_sq, 2.0*z_1.x*z_1.y);
-
-    // the recurrence equation
-    z = parameterSet1.x*z_0_sq + complexNumber + parameterSet1.y*z_1_sq
-    + parameterSet1.z*complexMultiplikation(z_1_sq, z_2) + parameterSet2.x*complexMultiplikation(z_1_sq, z_0)
-    + parameterSet2.y*complexMultiplikation(z_2, z_0) + parameterSet2.z*complexMultiplikation(z_1, z_2);
-
-    float z_0_mag = x_0_sq + y_0_sq;
-    float z_1_mag = x_1_sq + y_1_sq;
-
-    if(z_0_mag > 12.0){
-      float frac = (12.0 - z_1_mag) / (z_0_mag - z_1_mag);
-      alpha = (float(i) - 1.0 + frac)/200.0; // should be same as max iterations
-      break;
-    }
+vec4 getMandelbrot(vec2 c) {
+  float a = 0.0, b = 0.0;
+  int maxIteration = 200;
+  for (int i = 0; i < maxIteration; i++) {
+     float aNew = a*a - b*b + c.x;
+     float bNew = 2.0 * a * b + c.y;
+     if (aNew > 12.0 || bNew > 12.0) {
+       // not part of the mandelbrot set -> colored
+        return vec4(0.9/float(i+1), 0.3/float(i+1), 0.2/float(i+1), 1.0);
+     }
+     a = aNew;
+     b = bNew;
   }
-
-  return alpha;
+  return vec4(0.0, 0.0, 0.0, 1.0);
 }
 
-void main(){ // gl_FragCoord in [0,1]
-  vec2 uv = zoom * vec2(aspect, 1.0) * gl_FragCoord.xy / res + offset;
-  float s = 1.0 - mandelbrot(uv);
-
-  vec3 coord = vec3(s, s, s);
-  gl_FragColor = vec4(pow(coord, color), 1.0);
+void main() {
+  vec2 c = zoom * vec2(aspect, 1.0) * gl_FragCoord.xy / res + offset;
+  vec4 col = getMandelbrot(c);
+  gl_FragColor = col;
 }
 
 `
